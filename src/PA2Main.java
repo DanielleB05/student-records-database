46ce35e71e169c9ace4bfbecf00002be18a5c336
Danielle Boisseranc <danielleb@Danielles-MacBook-Air.local>
2025-10-12 11:22:39 -0700

    Added argument check for CSV files

--- src/PA2Main.java
+++ src/PA2Main.java
@@ -3,6 +3,11 @@ public class PA2Main
 {
     public static void main(String[] args)
     {
-        System.out.println("PA2Main start");
+        if (args.length == 0)
+        {
+            System.out.println("Usage: java PA2Main <file1.csv> <file2.csv> ...");
+            return;
+        }
+        System.out.println("PA2Main start.");
     }
 }
