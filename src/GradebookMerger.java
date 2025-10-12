921ad1ba6e84b9be9c83e5998c4792e53c10be25
Danielle Boisseranc <danielleb@Danielles-MacBook-Air.local>
2025-10-12 14:59:15 -0700

    Further implemented GradebookMerger using Scanner to read CSV

--- src/GradebookMerger.java
+++ src/GradebookMerger.java
@@ -1,24 +1,71 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GradebookMerger 
 {
-    private ArrayList<String> csvFiles;
+    private ArrayList<StudentRecord> students = new ArrayList<>();
 
-    public GradebookMerger()
+    public void addFile(String filename) throws FileNotFoundException
     {
-        csvFiles = new ArrayList<>();
+        Scanner sc = new Scanner(new File(filename));
+
+        if (sc.hasNextLine())
+        {
+            sc.nextLine();
+        }
+
+        if (sc.hasNextLine())
+        {
+            sc.nextLine();
+        }
+
+        String category = filename.contains("_") ? filename.split("_")[0] : filename;
+        
+        while (sc.hasNextLine())
+        {
+            Scanner line = new Scanner(sc.nextLine());
+            String id = line.next();
+            String name = line.next();
+            double score = line.nextDouble();
+
+            StudentRecord student = findStudent(id);
+
+            if (student == null)
+            {
+                student = new StudentRecord(id, name);
+                students.add(student);
+            }
+
+            student.addScore(category, score);
+            line.close();
+        }
+
+        sc.close();
+        System.out.println("Read file: " + filename);
     }
 
-    public void addFile(String filename)
+    private StudentRecord findStudent(String id)
     {
-        csvFiles.add(filename);
-        System.out.println("file added: " + filename);
+        for (StudentRecord s : students)
+        {
+            if (s.getId().equals(id))
+            {
+                return s;
+            }
+        }
+
+        return null;
     }
 
     public void writeOutputs(String detailsFile, String summaryFile)
     {
-        System.out.println("Writing output files.");
-        System.out.println("Details file: " + detailsFile);
-        System.out.println("Summary file: " + summaryFile);
+        System.out.println("Would write outputs to: " + detailsFile + ", " + summaryFile);
+    }
+
+    public ArrayList<StudentRecord> getStudents()
+    {
+        return students;
     }
 }
