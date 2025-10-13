2802949a4667f6f0951a523ce8479fdb0096e63c
Danielle Boisseranc <danielleb@Danielles-MacBook-Air.local>
2025-10-13 15:37:07 -0700

    Implemented writeDetails() to generate detailed CSV for students

--- src/CSVFile.java
+++ src/CSVFile.java
@@ -4,5 +4,24 @@ import java.util.ArrayList;
 
 public class CSVFile 
 {
-    
+    public static void writeDetails(String filename, ArrayList<StudentRecord> students) throws IOException
+    {
+        FileWriter writer = new FileWriter(filename);
+        writer.write("ID, Name, Category, Score");
+
+        for (StudentRecord s : students)
+        {
+            ArrayList<String> categories = s.getCategories();
+            ArrayList<Double> scores = s.getScores();
+
+            for (int i = 0; i < categories.size(); i++)
+            {
+                writer.write(s.getId() + "," + s.getName() + "," + categories.get(i) + "," + scores.get(i));
+            }
+        }
+
+        writer.close();
+
+        System.out.println("Details written to " + filename);
+    }
 }
