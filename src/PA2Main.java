128407481433e26615062adfa3876164b1598382
Danielle Boisseranc <danielleb@Danielles-MacBook-Air.local>
2025-10-13 14:56:31 -0700

    Updated PA2Main to call addFile() and handle file inputs

--- src/PA2Main.java
+++ src/PA2Main.java
@@ -1,20 +1,26 @@
+import java.io.FileNotFoundException;
 
 public class PA2Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
         if (args.length == 0)
         {
             System.out.println("Usage: java PA2Main <file1.csv> <file2.csv> ...");
             return;
         }
+
         System.out.println("PA2Main start.");
 
-        GradebookManager merger = new GradebookManager();
+        GradebookManager manager = new GradebookManager();
 
         for (String filename : args)
         {
             System.out.println("Reading file: " + filename);
+            manager.addFile(filename);
         }
+
+        System.out.println("All files read successfully.");
+        manager.writeOutputs("details.csv", "summary.csv");
     }
 }
