93dff2347a61b3ddd50eae457196c9282b0cfeb5
Danielle Boisseranc <danielleb@Danielles-MacBook-Air.local>
2025-10-14 13:23:33 -0700

    Implemented StudentRecords constructor to initialize class fields

--- src/StudentRecords.java
+++ src/StudentRecords.java
@@ -6,4 +6,13 @@ public class StudentRecords
     private String name;
     private ArrayList<Double> scores;
     private ArrayList<String> categories;
+
+    public StudentRecords(String id, String name)
+    {
+        this.id = id;
+        this.name = name;
+        this.scores = new ArrayList<>();
+        this.categories = new ArrayList<>();
+    }
+
 }
