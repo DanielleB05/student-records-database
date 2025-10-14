e018135712c32f763dfe98de59db017b2d03b1a9
Danielle Boisseranc <danielleb@Danielles-MacBook-Air.local>
2025-10-14 13:06:18 -0700

    Added StudentRecords class to include id, name, scores, and categories

--- src/StudentRecords.java
+++ src/StudentRecords.java
@@ -2,5 +2,8 @@ import java.util.ArrayList;
 
 public class StudentRecords 
 {
-    
+    private String id;
+    private String name;
+    private ArrayList<Double> scores;
+    private ArrayList<String> categories;
 }
